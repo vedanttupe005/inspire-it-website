d5a333571f8d087b090ca33437de1e08f75db6a5
vedanttupe005 <vedanttupe005@gmail.com>
2025-11-14 22:31:45 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/plans")
+def plans():
+    return render_template("plans.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
